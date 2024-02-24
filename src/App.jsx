b013f8ef69ff4880a4be07f98bcc29db26863ef0
Chukwuemerie <chukwuemerie@fincra.com>
2024-02-24 03:06:22 +0100

    created dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,34 @@
-import "./App.css"
-import {Routes, Route } from "react-router-dom"
-import Header from "./Components/Header/Header"
-import Footer from "./Components/Footer/Footer"
-import AboutPage from "./Pages/About/About"
-import Service from "./Pages/Service/Service"
-import Home from "./Pages/Home/Home"
-import Contact from "./Pages/Contact/Contact"
-import Signup from "./Pages/SignUp/Signup"
-import Pricing from './Pages/Pricing/Pricing'
-import Login from './Pages/Auth/Login/Login'
-
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import AboutPage from "./Pages/About/About";
+import Service from "./Pages/Service/Service";
+import Home from "./Pages/Home/Home";
+import Contact from "./Pages/Contact/Contact";
+import Signup from "./Pages/SignUp/Signup";
+import Pricing from "./Pages/Pricing/Pricing";
+import Login from "./Pages/Auth/Login/Login";
+// import DashboardLayout from "./Pages/Dashboard/Dashboard";
+import Clients from "./Components/route/clients"
+import Dashboards from "./Components/route/dashboard";
+import Setting from "./Components/route/settings";
 
 function App() {
   return (
     <>
-
-    <Routes>
-      <Route path="/"  element={<Home/>}/>
-      <Route path="/service"  element={<Service/>} />
-      <Route path="/about"  element={<AboutPage/>} />
-      <Route path="/contact"  element={<Contact/>} />
-      <Route path="/signup"  element={<Signup/>} />
-      <Route path="/pricing"  element={<Pricing/>} />
-      <Route path="/login"  element={<Login/>} />
-    </Routes> 
-       
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboards" element={<Dashboards />} />
+        <Route path="/clients" element={<Clients />} />
+        <Route path="/settings" element={<Setting />} />
+      </Routes>
     </>
   );
 }
