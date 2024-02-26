22d834c2a21a88146c4286a95b4c468503b7fff0
Emmanuella~ <obodoemmanuella@gmail.com>
2024-02-26 09:53:49 -0800

    wip on settings page

--- src/Components/route/settings.jsx
+++ src/Components/route/settings.jsx
@@ -1,11 +1,13 @@
 import DashboardLayout from "../../Pages/Dashboard/Dashboard";
 import PageHeader from "../Header/PageHeader";
+import "./Elsa.css"
 
 export default function Setting() {
   return (
     <>
       <DashboardLayout>
         <PageHeader title="Settings Page" />
+        <div className="settings    h-40 w-full "> elsaa</div>
       </DashboardLayout>
     </>
   );
