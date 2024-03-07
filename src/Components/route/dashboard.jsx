bbb402741b71917cd5630807bfbd9f88315064ac
Emmanuella~ <obodoemmanuella@gmail.com>
2024-03-07 01:13:39 -0800

    text/color

--- src/Components/route/dashboard.jsx
+++ src/Components/route/dashboard.jsx
@@ -52,37 +52,53 @@ export default function Dashboards() {
         <section className=" h-[77vh]  w-full flex items-center justify-center  ">
           <div className="w-[100%]  lg:h-5/6  h-[100%] flex space-x-4 lg:justify-start justify-center ">
             <div className="w-1/5   space-y-4  items-center  py-4  rounded-sm  shadow-md   flex-col hidden lg:flex ">
-              <div className="w-4/5 h-24 bg-slate-400 rounded  items-center -center flex flex-col py-2">
+              <div className="w-4/5 h-24  rounded  items-center -center flex flex-col py-2" style={{
+                boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px"
+              }}>
                 <h3 className="text-lg">Total schedules</h3>
 
-                <p className="text-sm text-white "> ({schedule.length}) </p>
+                <p className="text-sm text-#003482ff "> ({schedule.length}) </p>
               </div>
-              <div className="w-4/5 h-24  bg-orange-300 rounded  items-center justify-center flex flex-col">
-                <h3 className="text-lg ">Total Clients</h3>
-                <p className="text-sm text-white "> ( {client.length} )</p>
+              <div className="w-4/5 h-24  bg-white rounded  items-center justify-center flex flex-col"style={{
+                boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px"
+              }}
+              
+              >
+                <h3 className="text-lg">Total Clients</h3>
+                <p className="text-sm text-#003482ff "> ( {client.length} )</p>
               </div>
-              <div className="w-4/5 h-24 bg-slate-200 rounded  items-center justify-center flex flex-col">
+              <div className="w-4/5 h-24  rounded  items-center justify-center flex flex-col" style={{
+                boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px"
+              }}>
                 <h3 className="text-base first-letter:">Total schedules</h3>
 
-                <p className="text-sm text-white "> ({schedule.length}) </p>
+                <p className="text-sm text-#003482ff "> ({schedule.length}) </p>
               </div>
             </div>
 
-            <div className="lg:w-3/4 w-full  space-y-4 h-5/6  flex flex-col ">
-              <div className="h-[25%] flex space-x-4 py-4  items-center justify-center text-center ">
-                <div className="w-4/5 h-24 bg-slate-400 rounded  items-center justify-center flex flex-col py-2">
+            <div className="lg:w-3/4 w-full  space-y-4 h-5/6  flex flex-col " style={{
+                boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px"
+              }}>
+              <div className="h-[25%] flex space-x-4 py-4  items-center justify-center text-center " >
+                <div className="w-4/5 h-24  rounded  items-center justify-center flex flex-col py-2" style={{
+                boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px"
+              }}>
                   <MdPayment style={{ color: "#003482ff" }} />
                   <Link to="/payments" className="link">
                     <p>Get a payment plan</p>
                   </Link>
                 </div>
-                <div className="w-4/5 h-24 bg-slate-400 rounded  items-center justify-center flex flex-col py-2">
+                <div className="w-4/5 h-24  rounded  items-center justify-center flex flex-col py-2" style={{
+                boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px"
+              }}>
                   <IoPersonAddOutline style={{ color: "#003482ff" }} />
                   <Link to="/clients" className="link">
                     <p>Add a client</p>
                   </Link>
                 </div>
-                <div className="w-4/5 h-24 bg-slate-400 rounded  items-center  justify-center flex flex-col py-2">
+                <div className="w-4/5 h-24 rounded  items-center  justify-center flex flex-col py-2" style={{
+                boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px"
+              }}>
                   <GrScheduleNew style={{ color: "#003482ff" }} />
                   <Link to="/schedules" className="link">
                     <p>Schedule an appointment</p>
