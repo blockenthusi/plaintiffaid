1cc61afa88260743835931c8441b39951a78c720
oluomojossy <oluomojossy@gmail.com>
2024-05-18 21:46:13 +0100

    price page

--- src/Pages/Pricing/Pricing.jsx
+++ src/Pages/Pricing/Pricing.jsx
@@ -1,11 +1,13 @@
 // import React from 'react'
 import "./Pricing.css";
-import { FaCheck } from "react-icons/fa";
 import Header from "../../Components/Header/Header";
-import PricingCard from "../../Components/pricingcard/PricingCard";
+// import PricingCard from "../../Components/pricingcard/PricingCard";
 import Footer from "../../Components/Footer/Footer";
 import { useState } from "react";
 import Dropdown from "../../Components/Dropdown/Dropdown";
+import { FaCheck } from "react-icons/fa" 
+import { Link } from 'react-router-dom'
+
 
 
 
@@ -25,39 +27,57 @@ const Pricing = () => {
       </section>
 
       <section className="pricing-card">
-        <p className="get-started">
-          Get started in minutes. Try it free for 7 days. Cancel anytime.
-        </p>
-
-        <div className="pricing-cards">
-          <PricingCard
-            plan="Free"
-            amount="0"
-            feature="Register new client data"
-            feature2="Review client data feeds"
-            feature3="Schedule management"
-            hideLastIcon={true}
-          />
-          <PricingCard
-            plan="Essentials"
-            amount="30,000"
-            feature="Review client data feeds"
-            feature2="Calendar integration"
-            feature3="Schedule management"
-            feature4="Automated notifications"
-            icon={FaCheck}
-          />
-      
-          <PricingCard
-            plan="Advance"
-            amount="60,000"
-            feature="Schedule management"
-            feature2="Calendar integration"
-            feature3="Schedule management"
-            feature4="Automated notifications"
-          />
+       <div className="price-card-container">
+        <div className="left-text">
+          <p className="first-text">Our system is completely <br/>robust for our users.</p>
+          <p className="second-text">Purchase a plan and have <br/>unlimited access to our solution  </p>
+          <p className="third-text">Click on Try for to get started</p>
         </div>
+        <div className="right-card">
+        <div className='card'>
+        <div className="plan">
+        <p className='free'>Essentials</p>
+        <p className='best'>Best for a team getting started.</p>
+        </div>
+
+        <div className="amount">
+        <p className='price'>₦60,000</p>
+        <p className='peruser'>per user, per month</p>
+        </div>
+
+        <div className="features">
+
+            <div className="features-icon">
+            <FaCheck fill='#FFA500' />
+            <p>Register new client data</p>
+            </div>
+            <div className="features-icon">
+            <FaCheck fill='#FFA500' />
+            <p>Calendar integration</p>
+            </div>
+            <div className="features-icon">
+            <FaCheck fill='#FFA500' />
+            <p>Schedule management</p>
+            </div>
+          <div className="features-icon">
+            <FaCheck fill="#FFA500" />
+            <p>Automated notifications</p>
+          </div>
+
+        </div>
+        <Link className='link' to="/signup">
+        <button>Try for free</button>
+        </Link>
+
+    </div>
+
+        </div>
+        
+       </div>
+
+
       </section>
+      
       <div className="nameless"></div>
       <Footer/>
     </>
